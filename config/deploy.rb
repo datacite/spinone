9081334e5ce156a5b191d05d1ffd91e844e965be
Martin Fenner <martin.fenner@datacite.org>
2015-11-29 23:52:45 +0100

    handle javascripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :ssh_options,
 set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
 
 # install/update npm modules and bower components
-set :npm_target_path, -> { release_path.join('frontend') }
+set :npm_target_path, -> { release_path.join('public') }
 
 # restart passenger method
 set :passenger_restart_with_touch, true
