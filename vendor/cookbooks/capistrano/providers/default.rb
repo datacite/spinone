07b001bd6101d1061fd5797903d456667154ddf8
Martin Fenner <martin.fenner@datacite.org>
2015-12-09 18:18:21 -0500

    updated capistrano cookbook

--- vendor/cookbooks/capistrano/providers/default.rb
+++ vendor/cookbooks/capistrano/providers/default.rb
@@ -12,8 +12,8 @@ action :deploy do
 end
 
 action :config do
-  # create shared folders
-  %W{ #{new_resource.name} #{new_resource.name}/shared #{new_resource.name}/shared/frontend #{new_resource.name}/shared/vendor #{new_resource.name}/shared/tmp #{new_resource.name}/shared/tmp/pids }.each do |dir|
+  # create folders
+  %W{ #{new_resource.name} #{new_resource.name}/frontend #{new_resource.name}/vendor #{new_resource.name}/log #{new_resource.name}/tmp #{new_resource.name}/tmp/pids }.each do |dir|
     directory "/var/www/#{dir}" do
       owner new_resource.user
       group new_resource.group
@@ -21,24 +21,16 @@ action :config do
       recursive true
     end
   end
-
-  # symlink current folder
-  link "/var/www/#{new_resource.name}/current" do
-    to "/var/www/#{new_resource.name}/shared"
-    owner new_resource.user
-    group new_resource.group
-    mode '0755'
-  end
 end
 
 action :bundle_install do
   run_context.include_recipe 'ruby'
 
-  if ::File.exist?("/var/www/#{new_resource.name}/current/Gemfile")
+  if ::File.exist?("/var/www/#{new_resource.name}/Gemfile")
     # make sure we can use the bundle command
     execute "bundle install" do
       user new_resource.user
-      cwd "/var/www/#{new_resource.name}/shared"
+      cwd "/var/www/#{new_resource.name}"
       if new_resource.rails_env == "development"
         command "bundle config --delete without --no-deployment && bundle install --path vendor/bundle"
       else
@@ -51,9 +43,9 @@ end
 action :npm_install do
   run_context.include_recipe 'nodejs'
 
-  if ::File.exist?("/var/www/#{new_resource.name}/current/frontend/package.json")
+  if ::File.exist?("/var/www/#{new_resource.name}/frontend/package.json")
     # create directory for npm packages
-    directory "/var/www/#{new_resource.name}/shared/frontend/node_modules" do
+    directory "/var/www/#{new_resource.name}/frontend/node_modules" do
       owner new_resource.user
       group new_resource.group
       mode '0755'
@@ -64,7 +56,7 @@ action :npm_install do
     # we need to set $HOME because of a Chef bug: https://tickets.opscode.com/browse/CHEF-2517
     execute "npm install" do
       user new_resource.user
-      cwd "/var/www/#{new_resource.name}/current/frontend"
+      cwd "/var/www/#{new_resource.name}/frontend"
       environment ({ 'HOME' => ::Dir.home(new_resource.user), 'USER' => new_resource.user })
       action :run
     end
@@ -74,28 +66,19 @@ end
 action :consul_install do
   # install consul
   run_context.include_recipe 'consul'
-
-  # monitor nginx service
-  if node['recipes'].include?('passenger_nginx')
-    consul_definition 'nginx' do
-      type 'check'
-      parameters(http: "http://#{node['fqdn']}:80", interval: '10s', timeout: '2s')
-      notifies :reload, 'consul_service[consul]', :delayed
-    end
-  end
 end
 
 action :precompile_assets do
   run_context.include_recipe 'nodejs'
   run_context.include_recipe 'ruby'
 
-  if ::File.exist?("/var/www/#{new_resource.name}/current/Gemfile")
+  if ::File.exist?("/var/www/#{new_resource.name}/Gemfile")
     # make sure we can use the bundle command
 
     execute "bundle exec rake assets:precompile" do
       user new_resource.user
       environment 'RAILS_ENV' => new_resource.rails_env
-      cwd "/var/www/#{new_resource.name}/current"
+      cwd "/var/www/#{new_resource.name}"
       not_if { new_resource.rails_env == "development" }
     end
   end
@@ -107,7 +90,7 @@ action :ember_build do
 
   # provide Rakefile if it doesn't exist, e.g. during testing
   cookbook_file "Rakefile" do
-    path "/var/www/#{new_resource.name}/current/Rakefile"
+    path "/var/www/#{new_resource.name}/Rakefile"
     owner new_resource.user
     group new_resource.group
     cookbook "capistrano"
@@ -117,33 +100,40 @@ action :ember_build do
   execute "bundle exec rake ember:build" do
     user new_resource.user
     environment 'RAILS_ENV' => new_resource.rails_env
-    cwd "/var/www/#{new_resource.name}/current"
+    cwd "/var/www/#{new_resource.name}"
   end
 end
 
 action :migrate do
   run_context.include_recipe 'ruby'
 
-  if ::File.exist?("/var/www/#{new_resource.name}/current/config/database.yml")
+  if ::File.exist?("/var/www/#{new_resource.name}/config/database.yml")
     # run database migrations
     execute "bundle exec rake db:migrate" do
       user new_resource.user
       environment 'RAILS_ENV' => new_resource.rails_env
-      cwd "/var/www/#{new_resource.name}/current"
+      cwd "/var/www/#{new_resource.name}"
     end
 
     # load/reload seed data
     execute "bundle exec rake db:seed" do
       user new_resource.user
       environment 'RAILS_ENV' => new_resource.rails_env
-      cwd "/var/www/#{new_resource.name}/current"
+      cwd "/var/www/#{new_resource.name}"
     end
+  end
+end
+
+action :whenever do
+  execute "whenever" do
+    cwd  "/var/www/#{new_resource.name}"
+    command "whenever --update-crontab -i #{new_resource.name}"
   end
 end
 
 action :restart do
   execute "restart" do
-    cwd  "/var/www/#{new_resource.name}/current"
+    cwd  "/var/www/#{new_resource.name}"
     command "mkdir -p tmp && touch tmp/restart.txt"
   end
 end
--- vendor/cookbooks/capistrano/providers/template.rb
+++ vendor/cookbooks/capistrano/providers/template.rb
@@ -15,14 +15,14 @@ action :create do
     if i + 1 < files.length
       # create parent folders of file with correct permissions
       dir = files[0..i].join("/")
-      directory "/var/www/#{new_resource.application}/shared/#{dir}" do
+      directory "/var/www/#{new_resource.application}/#{dir}" do
         owner new_resource.user
         group new_resource.group
         mode '0755'
       end
     else
       # create file from template
-      template "/var/www/#{new_resource.application}/shared/#{new_resource.name}" do
+      template "/var/www/#{new_resource.application}/#{new_resource.name}" do
         source new_resource.source
         variables(
           :application => new_resource.application,
