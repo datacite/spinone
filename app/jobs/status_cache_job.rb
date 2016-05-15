b7094c2e12f50fcaea12a97ae5f3bfba79725c33
Martin Fenner <martin.fenner@datacite.org>
2016-05-15 16:53:40 +0200

    added job for status cache

---
+++ app/jobs/status_cache_job.rb
@@ -0,0 +1,13 @@
+class StatusCacheJob < ActiveJob::Base
+  queue_as :critical
+
+  rescue_from ActiveJob::DeserializationError, ActiveRecord::ConnectionTimeoutError do
+    retry_job wait: 5.minutes, queue: :default
+  end
+
+  def perform
+    ActiveRecord::Base.connection_pool.with_connection do
+      Status.create
+    end
+  end
+end
