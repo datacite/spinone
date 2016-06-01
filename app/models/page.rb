8b345b469372fc64a4e5881a16e496fc75ce043c
Martin Fenner <martin.fenner@datacite.org>
2016-06-01 08:48:54 +0200

    added tags meta and license for pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,12 +1,13 @@
 class Page < Base
-  attr_reader :id, :author, :title, :container_title, :description, :tags, :issued, :updated_at
+  attr_reader :id, :author, :title, :container_title, :description, :license, :tags, :issued, :updated_at
 
   def initialize(attributes)
     @id = attributes.fetch("url").underscore.dasherize
-    @author = attributes.fetch("author", nil)
+    @author = attributes.fetch("author", [])
     @title = attributes.fetch("title", nil)
-    @container_title = attributes.fetch("container_title", nil)
+    @container_title = attributes.fetch("container-title", nil)
     @description = attributes.fetch("description", nil)
+    @license = attributes.fetch("license", nil)
     @tags = attributes.fetch("tags", [])
     @issued = attributes.fetch("issued", nil)
     @updated_at = @issued
@@ -29,8 +30,15 @@ class Page < Base
     else
       items = items.select { |i| i.values.join("\n").downcase.include?(options[:q]) } if options[:q]
       items = items.select { |i| Array(i["tags"]).include?(options[:tag]) } if options[:tag]
+      meta = { total: items.length, tags: parse_meta(items) }
+      { data: parse_items(items), meta: meta }
+    end
+  end
 
-      { data: parse_items(items), meta: { total: items.length } }
+  def self.parse_meta(items)
+    items.reduce({}) do |sum, i|
+      i["tags"].each { |tag| sum[tag] = sum[tag].to_i + 1 }
+      sum
     end
   end
 
