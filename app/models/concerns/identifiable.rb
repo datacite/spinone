7a012e19981772149fb07f56cfc527f38f6d4e6f
Martin Fenner <martin.fenner@datacite.org>
2016-06-13 11:51:13 +0200

    use correct variable

--- app/models/concerns/identifiable.rb
+++ app/models/concerns/identifiable.rb
@@ -6,8 +6,8 @@ module Identifiable
       if /(http|https):\/\/(dx\.)?doi\.org\/(\w+)/.match(url)
         uri = Addressable::URI.parse(url)
         uri.path[1..-1].upcase
-      elsif id.starts_with?("doi:")
-        id[4..-1].upcase
+      elsif url.starts_with?("doi:")
+        url[4..-1].upcase
       end
     end
 
