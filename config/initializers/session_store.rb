a3e3a3c0c61ab7a34e37e931852d0ba1476d7964
Martin Fenner <martin.fenner@datacite.org>
2016-07-21 10:32:28 +0200

    expire cookies after 14 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: ENV['SESSION_KEY'], domain: :all
+Rails.application.config.session_store :cookie_store,
+  key: ENV['SESSION_KEY'],
+  secure: Rails.env.production?,
+  domain: :all,
+  expire_after: 14.days
