54b28788a4cbb91fd8c4ec9a0b1d15da99a9d90a
Martin Fenner <martin.fenner@datacite.org>
2016-10-03 13:44:57 +0200

    do cors preflight check upstream

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,8 +2,7 @@ class Api::BaseController < ActionController::Base
   # include base controller methods
   include Authenticable
 
-  before_filter :default_format_json,
-                :cors_preflight_check
+  before_filter :default_format_json
   after_filter :cors_set_access_control_headers, :set_jsonp_format
 
   # from https://github.com/spree/spree/blob/master/api/app/controllers/spree/api/base_controller.rb
@@ -18,16 +17,6 @@ class Api::BaseController < ActionController::Base
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
   end
 
   protected
