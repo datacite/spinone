de4f85395f7160e5beb4a6921209812dd08c9a16
Martin Fenner <martin.fenner@datacite.org>
2016-10-04 22:11:26 +0200

    updated event model

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,13 +1,13 @@
 class Api::EventsController < Api::BaseController
   def index
     @events = Event.where(params)
-    render json: @events[:data], meta: @events[:meta]
+    render jsonapi: @events[:data], meta: @events[:meta]
   end
 
   def show
     @event = Event.where(id: params[:id])
     fail ActiveRecord::RecordNotFound unless @event.present?
 
-    render json: @event[:data]
+    render jsonapi: @event[:data]
   end
 end
