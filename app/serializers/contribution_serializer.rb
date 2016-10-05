bb5c3a40cbf76b20daed95f15987522058c757ca
Martin Fenner <martin.fenner@datacite.org>
2016-10-05 22:03:29 +0200

    updated contribution serializer

--- app/serializers/contribution_serializer.rb
+++ app/serializers/contribution_serializer.rb
@@ -1,6 +1,9 @@
 class ContributionSerializer < ActiveModel::Serializer
   cache key: 'contribution'
-  attributes :subj_id, :obj_id, :credit_name, :orcid, :github, :author, :doi, :url, :title, :container_title, :source_id, :contributor_role_id, :type, :published, :issued, :updated ,:publisher_id
+  attributes :subj_id, :obj_id, :credit_name, :orcid, :github, :author, :doi, :url, :title, :container_title, :contributor_role_id, :work_type, :published, :issued, :updated
+
+  belongs_to :publisher, serializer: PublisherSerializer
+  belongs_to :source, serializer: SourceSerializer
 
   def updated
     object.updated_at
