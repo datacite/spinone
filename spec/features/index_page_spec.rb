bd063a256d4f872ee88507b0577d4acc62232df0
Martin Fenner <martin.fenner@datacite.org>
2016-10-09 14:39:06 +0200

    updated feature specs

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 describe "index", type: :feature, js: true, vcr: true do
   it 'show index page' do
     visit '/'
-    expect(page).to have_css ".panel-body", text: "Version History"
+    expect(page).to have_css ".content h2", text: "Version History"
   end
 
-  it 'show link to agents' do
+  it 'show link to admin menu' do
     sign_in
     visit '/'
-    expect(page).to have_css ".navbar-nav li a", text: "Agents"
+    expect(page).to have_css ".dropdown-toggle", text: "Admin"
   end
 
   it 'not authorized' do
     visit '/'
-    expect(page).not_to have_css ".navbar-nav li a", text: "Agents"
+    expect(page).not_to have_css ".dropdown-toggle", text: "Agents"
   end
 
   it 'invalid_credentials' do
     sign_in(credentials: "invalid")
     visit '/'
-    expect(page).not_to have_css ".navbar-nav li a", text: "Agents"
+    expect(page).not_to have_css ".dropdown-toggle", text: "Agents"
   end
 
   it 'role user' do
     sign_in(role: "user")
     visit '/'
-    expect(page).not_to have_css ".navbar-nav li a", text: "Agents"
+    expect(page).not_to have_css ".dropdown-toggle", text: "Agents"
   end
 end
