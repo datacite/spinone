468f5076fdc791b3a503bcbd09c577cdaaed1ccb
Martin Fenner <martin.fenner@datacite.org>
2016-10-09 15:55:48 +0200

    reconfigure sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,5 +4,4 @@ Sidekiq.configure_server do |config|
   config.options[:concurrency] = ENV["CONCURRENCY"].to_i
 end
 
-Sidekiq::Logging.logger = Syslog::Logger.new(ENV['APPLICATION'])
 Sidekiq::Logging.logger.level = Logger.const_get(ENV["LOG_LEVEL"].upcase)
