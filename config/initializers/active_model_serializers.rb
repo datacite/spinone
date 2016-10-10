6473a0125288b5830252e5eb9957fd3c37c22a0a
Martin Fenner <martin.fenner@datacite.org>
2016-10-10 09:33:00 +0200

    include data when sideloaded

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,5 +1,5 @@
 ActiveModel::Serializer.config.adapter = ActiveModelSerializers::Adapter::JsonApi
-ActiveModelSerializers.config.include_data_default = :if_sideloaded
+ActiveModel::Serializer.config.include_data_default = :if_sideloaded
 ActiveSupport.on_load(:action_controller) do
   require 'active_model_serializers/register_jsonapi_renderer'
 end
