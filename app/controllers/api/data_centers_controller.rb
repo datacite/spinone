9ab0d88226006f004a6555bec88c5c9fa83f58db
Martin Fenner <martin.fenner@datacite.org>
2017-01-24 12:53:24 +0100

    added person and data-center api endpoints

---
+++ app/controllers/api/data_centers_controller.rb
@@ -0,0 +1,24 @@
+class Api::DataCentersController < Api::BaseController
+  before_filter :set_include
+
+  def set_include
+    if params[:include].present?
+      @include = params[:include].split(",").map { |i| i.downcase.underscore }.join(",")
+      @include = [@include]
+    else
+      @include = nil
+    end
+  end
+
+  def index
+    @data_centers = Publisher.where(params)
+    render jsonapi: @data_centers[:data], meta: @data_centers[:meta], include: @include
+  end
+
+  def show
+    @data_center = Publisher.where(id: params[:id])
+    fail ActiveRecord::RecordNotFound unless @data_center.present?
+
+    render jsonapi: @data_center[:data], include: @include
+  end
+end
---
+++ app/serializers/data_center_serializer.rb
@@ -0,0 +1,10 @@
+class DataCenterSerializer < ActiveModel::Serializer
+  attributes :title, :other_names, :prefixes, :member_id, :registration_agency_id, :updated
+
+  belongs_to :member, serializer: MemberSerializer
+  belongs_to :registration_agency, serializer: RegistrationAgencySerializer
+
+  def updated
+    object.updated_at
+  end
+end
