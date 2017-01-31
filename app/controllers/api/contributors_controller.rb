bab2c771be98c7071386543f58174afdca59c2a8
Martin Fenner <martin.fenner@datacite.org>
2017-01-31 13:02:38 +0100

    removed occurences of obsolete contributor

--- app/controllers/api/contributors_controller.rb
+++ app/controllers/api/contributors_controller.rb
@@ -1,11 +1,11 @@
 class Api::ContributorsController < Api::BaseController
   def index
-    @contributors = Contributor.where(params)
+    @contributors = Person.where(params)
     render jsonapi: @contributors[:data], meta: @contributors[:meta]
   end
 
   def show
-    @contributor = Contributor.where(id: params[:id])
+    @contributor = Person.where(id: params[:id])
     fail ActiveRecord::RecordNotFound unless @contributor.present?
 
     render jsonapi: @contributor[:data]
--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-describe Contributor, type: :model, vcr: true do
+describe Person, type: :model, vcr: true do
   it "contributors" do
-    contributors = Contributor.where(rows: 50)[:data]
+    contributors = Person.where(rows: 50)[:data]
     expect(contributors.length).to eq(50)
     contributor = contributors.first
     expect(contributor.literal).to eq("mne-tools")
   end
 
   it "contributors with query" do
-    contributors = Contributor.where(query: "0000-0002-4000-4167")[:data]
+    contributors = Person.where(query: "0000-0002-4000-4167")[:data]
     expect(contributors.length).to eq(1)
     contributor = contributors.first
     expect(contributor.family).to eq("Arend")
   end
 
   it "contributor" do
-    contributor = Contributor.where(id: "http://orcid.org/0000-0002-4000-4167")[:data]
+    contributor = Person.where(id: "http://orcid.org/0000-0002-4000-4167")[:data]
     expect(contributor.family).to eq("Arend")
   end
 end
