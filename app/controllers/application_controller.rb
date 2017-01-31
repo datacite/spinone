2778a02810d3f0964329849a9daa5c73b69bd3ec
Martin Fenner <martin.fenner@datacite.org>
2017-01-31 12:54:57 +0100

    added rack-miniprofiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,17 @@ class ApplicationController < ActionController::Base
 
   include ApplicationHelper
 
+  before_filter :miniprofiler
+
   helper_method :current_user
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
+  end
+
+  private
+
+  def miniprofiler
+    Rack::MiniProfiler.authorize_request if current_user && current_user.is_admin?
   end
 end
