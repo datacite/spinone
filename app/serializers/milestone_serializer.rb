b1c5d423a01df7258735556b920fe457bb0175d0
Martin Fenner <martin.fenner@datacite.org>
2017-06-16 10:49:42 +0900

    markdown parser for milestones. datacite/datacite#100

--- app/serializers/milestone_serializer.rb
+++ app/serializers/milestone_serializer.rb
@@ -1,6 +1,10 @@
 class MilestoneSerializer < ActiveModel::Serializer
   attributes :title, :description, :open_issues, :closed_issues, :year, :quarter, :created, :updated, :closed, :released
 
+  def description
+    GitHub::Markdown.render_gfm(object.description)
+  end
+
   def created
     object.created_at
   end
