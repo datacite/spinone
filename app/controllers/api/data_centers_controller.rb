279340409b2e5e65ffbd8466410743f321ff7bb9
Martin Fenner <mfenner@datacite.org>
2017-08-13 18:09:14 +0200

    always include member with data center

--- app/controllers/api/data_centers_controller.rb
+++ app/controllers/api/data_centers_controller.rb
@@ -6,7 +6,7 @@ class Api::DataCentersController < Api::BaseController
       @include = params[:include].split(",").map { |i| i.downcase.underscore }.join(",")
       @include = [@include]
     else
-      @include = nil
+      @include = ["member"]
     end
   end
 
