bd53ca8d3efca0ff4c0a0571b029bcd845245d7e
Martin Fenner <mfenner@datacite.org>
2017-10-23 10:01:30 +0200

    simplify configuration

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,33 @@ require 'securerandom'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# load ENV variables from .env file if it exists
+env_file = File.expand_path("../../.env", __FILE__)
+if File.exist?(env_file)
+  require 'dotenv'
+  Dotenv.load! env_file
+end
+
+# load ENV variables from container environment if json file exists
+# see https://github.com/phusion/baseimage-docker#envvar_dumps
+env_json_file = "/etc/container_environment.json"
+if File.exist?(env_json_file)
+  env_vars = JSON.parse(File.read(env_json_file))
+  env_vars.each { |k, v| ENV[k] = v }
+end
+
+# default values for some ENV variables
+ENV['APPLICATION'] ||= "spinone"
+ENV['HOSTNAME'] ||= "api.local"
+ENV['MEMCACHE_SERVERS'] ||= "localhost:11211"
+ENV['SITE_TITLE'] ||= "DataCite API"
+ENV['LOG_LEVEL'] ||= "info"
+ENV['BRACCO_URL'] ||= "https://doi.datacite.org"
+ENV['LUPO_URL'] ||= "https://api.datacite.org"
+ENV['CDN_URL'] ||= "https://assets.datacite.org"
+ENV['GITHUB_URL'] ||= "https://github.com/datacite/spinone"
+ENV['TRUSTED_IP'] ||= "127.0.0.0/8"
+
 module Spinone
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -49,13 +76,22 @@ module Spinone
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
     config.lograge.enabled = true
+    config.log_level = ENV['LOG_LEVEL'].to_sym
+
+    # Use memcached as cache store
+    config.cache_store = :dalli_store, nil, { :namespace => ENV['APPLICATION'], :compress => true }
+
+    # compress responses with deflate or gzip
+    config.middleware.use Rack::Deflater
 
     # Configure the default encoding used in templates for Ruby.
     config.encoding = "utf-8"
 
+    # secret_key_base is not used by Rails API, as there are no sessions
+    config.secret_key_base = 'blipblapblup'
+
     # Configure sensitive parameters which will be filtered from the log file.
-    # TODO: do I need to add salt here?
-    config.filter_parameters += [:password, :authentication_token]
+    config.filter_parameters += [:password, :authentication_token, :jwt]
 
     # Skip validation of locale
     I18n.enforce_available_locales = false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # config.web_console.whitelisted_ips = ENV['TRUSTED_IP']
 end
+
+BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP']
