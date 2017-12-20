c40842b20725a7384d0b8b2b8f34e0f0af66179e
Martin Fenner <mfenner@datacite.org>
2017-12-20 08:36:24 +0100

    handle missing data in cache

--- app/models/concerns/cacheable.rb
+++ app/models/concerns/cacheable.rb
@@ -2,15 +2,10 @@ module Cacheable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def cached_data_centers
-      Rails.cache.fetch("data_centers", expires_in: 1.month) do
-        DataCenter.all[:data]
-      end
-    end
-
     def cached_members
       Rails.cache.fetch("members", expires_in: 1.day) do
-        Member.all[:data]
+        member = Member.all
+        member.present? ? member[:data] : []
       end
     end
 
