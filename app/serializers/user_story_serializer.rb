6f5806760be83a4b0b2cdb6747ca97a26f50c736
Martin Fenner <mfenner@datacite.org>
2018-03-05 12:33:04 +0100

    fix user_story type in jsonapi

--- app/serializers/user_story_serializer.rb
+++ app/serializers/user_story_serializer.rb
@@ -1,5 +1,6 @@
 class UserStorySerializer
   include FastJsonapi::ObjectSerializer
 
+  set_type "user-stories"
   attributes :id, :url, :title, :description, :comments, :projects, :stakeholders, :state, :inactive, :milestone, :created, :updated, :closed
 end
