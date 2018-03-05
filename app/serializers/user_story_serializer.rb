1754f2949bcf3d085d27ad9a0fc60c522cce98b9
Martin Fenner <mfenner@datacite.org>
2018-03-05 10:45:44 +0100

    fixed typo

--- app/serializers/user_story_serializer.rb
+++ app/serializers/user_story_serializer.rb
@@ -1,5 +1,5 @@
 class UserStorySerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :url, :title, :description, :comments, :categories, :stakeholders, :state, :inactive, :milestone, :created, :updated, :closed
+  attributes :id, :url, :title, :description, :comments, :projects, :stakeholders, :state, :inactive, :milestone, :created, :updated, :closed
 end
