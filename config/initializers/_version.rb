c8de7b1ad0012906e26321f044eb9c33d6e2600b
Martin Fenner <martin.fenner@datacite.org>
2018-12-29 16:16:48 +0100

    app version

---
+++ config/initializers/_version.rb
@@ -0,0 +1,5 @@
+module Spinone
+  class Application
+    VERSION = "2.0.43"
+  end
+end
--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,6 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
+  config.notify_release_stages = %w(stage production)
+  config.app_version = Spinone::Application::VERSION
+  config.auto_capture_sessions = true
 end
