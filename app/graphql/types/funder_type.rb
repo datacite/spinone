4488ec9129794e156ef0989c2de7c0bac2714fea
Martin Fenner <martin.fenner@datacite.org>
2019-10-10 07:12:34 +0200

    Include type in funder graphql. datacite/lupo#346

--- app/graphql/types/funder_type.rb
+++ app/graphql/types/funder_type.rb
@@ -6,10 +6,15 @@ class FunderType < BaseObject
   description "Information about funders"
 
   field :id, ID, null: false, description: "Crossref Funder ID"
+  field :type, String, null: false, description: "The type of the item."
   field :name, String, null: false, description: "The name of the funder."
   field :alternate_name, [String], null: true, description: "An alias for the funder."
   field :address, AddressType, null: true, description: "Physical address of the funder."
 
+  def type
+    "Funder"
+  end
+  
   def address
     { "type" => "postalAddress",
       "address_country" => object.country.to_h.fetch("name", nil) }
